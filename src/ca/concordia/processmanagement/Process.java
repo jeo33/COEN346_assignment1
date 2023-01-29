c862477f78620a8b6b45e9d8c6cffe107f72fe93
gjpwewq <gjpwewa@hotmail.com>
2023-01-29 03:21:17 -0500

    add existing priject to coen346

---
+++ src/ca/concordia/processmanagement/Process.java
@@ -0,0 +1,14 @@
+package ca.concordia.processmanagement;
+
+public class Process {
+
+
+    private static final int MIN_PID = 300;
+    private static final int MAX_PID = 500;
+
+    public void allocateMap()throws Exception
+    public int allocatePid() throws Exception
+    public void releasePid(int pid)
+
+
+}
---
+++ src/ca/concordia/processmanagement/ProcessControlBlock.java
@@ -0,0 +1,7 @@
+package ca.concordia.processmanagement;
+
+public class ProcessControlBlock {
+
+    int PID;
+    int ProcessStatus;//0 1 2 3
+}
